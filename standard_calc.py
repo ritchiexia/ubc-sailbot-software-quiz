8cd76366fc16e63260dd857f410f7f90e33e7839
ritchiexia <ritchie.xia@gmail.com>
2022-01-15 20:04:32 -0800

    Implemented functions and tests

--- standard_calc.py
+++ standard_calc.py
@@ -1,3 +1,6 @@
+from math import fmod
+
+
 def bound_to_180(angle):
     """Bounds the provided angle between [-180, 180) degrees.
 
@@ -7,7 +10,11 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return 0
+
+    angle = fmod(angle + 180, 360)
+    if angle < 0:
+        angle += 360
+    return angle - 180
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
@@ -21,4 +28,12 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
-    return True
+
+    f = bound_to_180(first_angle)
+    m = bound_to_180(middle_angle)
+    s = bound_to_180(second_angle)
+
+    f, s = sorted((f, s))
+    if f - s > 180:
+        [f, m, s] = [f, m, s].map(lambda x: -x)
+    return f < m < s
